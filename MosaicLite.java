ac6328c2e87ad549bfbe13429320797ac0618806
Anthony Garza <anthonylgarza@lewisu.edu>
2019-02-06 22:23:10 -0600

    implement Jframe

---
+++ MosaicLite.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+class MosaicLiteFrame extends JFrame {
+    public MosaicLiteFrame() {
+        setBounds(200,200,1200,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+public class MosaicLite {
+    public static void main(String [] args) {
+        System.out.println("Starting MosaicLite...");
+
+        MosaicLiteFrame myMosaicLiteFrame = new MosaicLiteFrame();
+        myMosaicLiteFrame.setVisible(true);
+    }
+}
